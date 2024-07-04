f1868cbb335ac1d80903fcc75e3b22c9765fb275
Fabricio Loyola <fabriemiloyola99@gmail.com>
2024-07-04 16:42:00 -0300

    second commit

--- script.js
+++ script.js
@@ -39,3 +39,49 @@ document.addEventListener('DOMContentLoaded', () => {
     
     type();
 });
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// const $form = document.querySelector('#form')
+
+// $form.addEventListener('submit', handleSubmit)
+
+// async function handleSubmit(event) {
+//     event.preventDefault()
+//     const form = new FormData(this)
+//     const response = await fetch(this.action, {
+//         method: this.metod,
+//         body: form,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     })
+//     if(response.ok) {
+//         this.reset()
+//         alert('Gracias por contactarme. Te voy a estar comunicando a la brevedad!')
+//     }
+// }
